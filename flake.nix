8d4cccdbe9d4b005018c7e01fcf8ac2517764c03
Jörg Kütemeier <joerg@kuetemeier.de>
2025-01-16 14:16:43 +0100

    wip: Add nix-units for testing

--- flake.nix
+++ flake.nix
@@ -3,7 +3,15 @@
 
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
+    # flake-parts use nixos-unstable
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+
+    # Use nixos-unstable as default
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    nix-unit.url = "github:nix-community/nix-unit";
+    nix-unit.inputs.nixpkgs.follows = "nixpkgs";
+    nix-unit.inputs.flake-parts.follows = "flake-parts";
   };
 
   outputs = inputs @ {
@@ -13,12 +21,14 @@
   }:
     flake-parts.lib.mkFlake {inherit inputs;} {
       imports = [
-        # To import a flake module
-        # 1. Add foo to inputs
-        # 2. Add foo as a parameter to the outputs function
-        # 3. Add here: foo.flakeModule
+        inputs.nix-unit.modules.flake.default
       ];
-      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
       perSystem = {
         config,
         self',
@@ -33,6 +43,25 @@
 
         # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
         packages.default = pkgs.hello;
+
+        nix-unit.inputs = {
+          # NOTE: a `nixpkgs-lib` follows rule is currently required
+          inherit (inputs) nixpkgs flake-parts nix-unit;
+        };
+        # Tests specified here may refer to system-specific attributes that are
+        # available in the `perSystem` context
+        nix-unit.tests = {
+          "test integer equality is reflexive" = {
+            expr = "123";
+            expected = "123";
+          };
+          "frobnicator" = {
+            "testFoo" = {
+              expr = "foo";
+              expected = "foo";
+            };
+          };
+        };
       };
       flake = {
         # The usual flake attributes can be defined here, including system-
@@ -46,7 +75,14 @@
           };
         };
 
-        defaultTemplate = self.templates.base;
+        templates.default = self.templates.base;
+
+        # System-agnostic tests can be defined here, and will be picked up by
+        # `nix flake check`
+        tests.testBar = {
+          expr = "bar";
+          expected = "bar";
+        };
       };
     };
 }
