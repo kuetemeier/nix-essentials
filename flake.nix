8193fc3139258a7031d324b50212cee8217a35bc
Jörg Kütemeier <joerg@kuetemeier.de>
2025-01-29 21:32:50 +0100

    Feature: Add flakeModules and refactor dev

--- flake.nix
+++ flake.nix
@@ -20,172 +20,48 @@
     self,
     ...
   }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      imports = [
-        inputs.flake-parts.flakeModules.partitions
-      ];
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ];
-      perSystem = {
-        config,
-        pkgs,
+    flake-parts.lib.mkFlake {inherit inputs;}
+    (
+      {
+        self,
+        lib,
+        withSystem,
+        moduleWithSystem,
+        flake-parts-lib,
         ...
-      }: {
-        # }: let
-        #   checkFlatFlake =
-        #     pkgs.runCommand "checkFlatFlake" {
-        #       nativeBuildInputs = [
-        #         pkgs.nix
-        #       ];
-        #     } ''
-        #       mkdir $out
-        #       nix --sandbox false --extra-experimental-features nix-command \
-        #         run github:linyinfeng/flat-flake -- check ${self}
-        #     '';
-        # in {
-        # Per-system attributes can be defined here. The self' and inputs'
-        # module parameters provide easy access to attributes of the same
-        # system.
-
-        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-        packages.default = pkgs.hello;
-        # packages.checkFlatFlake = checkFlatFlake;
-
-        # checks = {
-        #   inherit flat-check;
-        # };
-      };
-      flake = {
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
-
-        # Configuration for flat-flake
-        # Allow some exceptions for the "flat" check of this flake
-        # GitHub: https://github.com/linyinfeng/flat-flake
-        flatFlake = {
-          # allowed = [
-          #   ["nix-unit" "nix-github-actions"]
-          # ];
+      }: let
+        # selfLib = import ./nix/lib {inherit inputs lib;};
+        inherit (flake-parts-lib) importApply;
+        flakeModules = {
+          dev = importApply ./flakeModules/dev {inherit withSystem moduleWithSystem importApply;};
         };
+        flakeModules.default = flakeModules.helixPackages;
+      in {
+        imports =
+          [
+            inputs.flake-parts.flakeModules.partitions
+          ]
+          ++ (with flakeModules; [
+            dev
+          ]);
 
-        templates = {
-          base = {
-            path = ./templates/base;
-            description = "Basic template for new nix projects with flake parts";
-          };
-        };
-
-        templates.default = self.templates.base;
-      };
-
-      # Extra things to load only when accessing development-specific
-      # attributes such as `checks`
-      partitionedAttrs.checks = "nedev";
-      partitionedAttrs.devShells = "nedev";
-      partitionedAttrs.tests = "nedev";
-      partitionedAttrs.formatter = "nedev";
-      partitions.nedev.extraInputsFlake = ./dev;
-      partitions.nedev.module = {inputs, ...}: {
-        imports = [
-          inputs.treefmt-nix.flakeModule
-          inputs.nix-unit.modules.flake.default
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+          "aarch64-darwin"
+          "x86_64-darwin"
         ];
-        perSystem = {
-          config,
-          pkgs,
-          inputs',
-          system,
-          lib,
-          ...
-        }: {
-          # Use `treefmt` for formatting
-          # GitHub: https://github.com/numtide/treefmt-nix
-          # Import treefmt config
-          treefmt.imports = [./dev/treefmt.nix];
 
-          formatter = config.treefmt.build.wrapper;
-
-          devShells.default = let
-            pythonEnv = pkgs.python3.withPackages (_ps: []);
-          in
-            pkgs.mkShell {
-              packages = [
-                pythonEnv
-                pkgs.difftastic
-                config.treefmt.build.wrapper
-                inputs'.nix-unit.packages.default
-                pkgs.just
-              ];
-              shellHook = ''
-                alias j=just
-                echo $'\e[1;32mWelcom to development Shell~\e[0m'
-                echo -n "Hint: 'just' is aliased to 'j'. "
-                echo "Just run 'j' for a list of possible commands"
-              '';
-            };
-
-          nix-unit.package = inputs'.nix-unit.packages.nix-unit;
-          nix-unit.inputs = {
-            # NOTE: a `nixpkgs-lib` follows rule is currently required
-            inherit (inputs) nixpkgs flake-parts treefmt-nix flake-compat;
-          };
-          nix-unit.allowNetwork = true;
-
-          checks = {
-            test =
-              pkgs.runCommandLocal "ensure-dependencies" {
-              } ''
-                echo "${inputs.flake-compat.rev}"
-                touch "$out"
-              '';
-          };
-
-          # checks = {
-          #   tests =
-          #     pkgs.runCommand "tests"
-          #     {
-          #       nativeBuildInputs = [
-          #         # inputs.nix-unit.packages.${system}.default
-          #         inputs'.nix-unit.packages.default
-          #       ];
-          #     } ''
-          #       export HOME="$(realpath .)"
-          #       # The nix derivation must be able to find all used inputs in the nix-store
-          #       # because it cannot download it during buildTime.
-          #       nix-unit --eval-store "$HOME" \
-          #       --extra-experimental-features flakes \
-          #       ${lib.concatStringsSep " " (lib.mapAttrsToList (name: value: "--override-input ${name} ${value}") (lib.filterAttrs (name: _: name != "self" && name != "nix-unit") inputs))} \
-          #       --flake ${self}#tests
-          #       touch $out
-          #     '';
-          # };
-
-          # Tests specified here may refer to system-specific attributes that are
-          # available in the `perSystem` context
-          nix-unit.tests = {
-            "test integer equality is reflexive" = {
-              expr = "123";
-              expected = "123";
-            };
-            "frobnicator" = {
-              "testFoo" = {
-                expr = "foo";
-                expected = "foo";
-              };
+        flake = {
+          templates = {
+            base = {
+              path = ./templates/base;
+              description = "Basic template for new nix projects with flake parts";
             };
           };
+
+          templates.default = self.templates.base;
         };
-      };
-      # flake = {
-      #   tests.testPass = {
-      #     expr = 3;
-      #     expected = 3;
-      #   };
-      # };
-    };
+      }
+    );
 }
