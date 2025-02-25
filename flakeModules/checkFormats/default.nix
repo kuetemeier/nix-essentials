8f250dcb9eea4ab178505d55cf1a4acd28c8fa65
Jörg Kütemeier <joerg@kuetemeier.de>
2025-02-25 12:43:36 +0100

    Refactor(checkFormats): Remove flake partitions

--- flakeModules/checkFormats/default.nix
+++ flakeModules/checkFormats/default.nix
@@ -9,38 +9,32 @@ localFlake: {
   inputs,
   ...
 }: {
-  # Extra things to load only when accessing development-specific
-  # attributes such as `checks`
-  partitionedAttrs.checks = "essentialsCheckFormats";
-  partitions.essentialsCheckFormats.extraInputsFlake = ./.;
-  partitions.essentialsCheckFormats.module = {inputs, ...}: {
-    imports = [
-      inputs.treefmt-nix.flakeModule
-    ];
-    perSystem = {
-      config,
-      pkgs,
-      inputs',
-      system,
-      lib,
-      ...
-    }: let
-      treefmtPkg = config.treefmt.build.wrapper;
-    in {
-      treefmt.imports = [./../formats/treefmt.nix];
-      checks = {
-        testEssentialFormats =
-          # Run treefmt in CI mode ( --no-cache --fail-onchange )
-          pkgs.runCommandLocal "checkEssentialsFormats" {
-          } ''
-            echo "Running nix-essentials format Checks..."
-            # echo "Running treefmt..."
-            # ${treefmtPkg}/bin/treefmt --ci
-            echo "Running editorconfig-check"
-            ${pkgs.editorconfig-checker}/bin/editorconfig-checker
-            touch "$out"
-          '';
-      };
+  imports = [
+    inputs.treefmt-nix.flakeModule
+  ];
+  perSystem = {
+    config,
+    pkgs,
+    inputs',
+    system,
+    lib,
+    ...
+  }: let
+    treefmtPkg = config.treefmt.build.wrapper;
+  in {
+    treefmt.imports = [./../formats/treefmt.nix];
+    checks = {
+      testEssentialFormats =
+        # Run treefmt in CI mode ( --no-cache --fail-onchange )
+        pkgs.runCommandLocal "checkEssentialsFormats" {
+        } ''
+          echo "Running nix-essentials format Checks..."
+          # echo "Running treefmt..."
+          # ${treefmtPkg}/bin/treefmt --ci
+          echo "Running editorconfig-check"
+          ${pkgs.editorconfig-checker}/bin/editorconfig-checker
+          touch "$out"
+        '';
     };
   };
 }
