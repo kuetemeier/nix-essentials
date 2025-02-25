e56760d57ee41e047818af8a9fe5c2e084e86938
Jörg Kütemeier <joerg@kuetemeier.de>
2025-02-25 12:44:27 +0100

    Refactor(formats): Remove flake partition

--- flakeModules/formats/default.nix
+++ flakeModules/formats/default.nix
@@ -9,30 +9,24 @@ localFlake: {
   inputs,
   ...
 }: {
-  # Extra things to load only when accessing development-specific
-  # attributes such as `checks`
-  partitionedAttrs.formatter = "essentialsFormats";
-  partitions.essentialsFormats.extraInputsFlake = ./.;
-  partitions.essentialsFormats.module = {inputs, ...}: {
-    imports = [
-      inputs.treefmt-nix.flakeModule
-    ];
-    perSystem = {
-      config,
-      pkgs,
-      inputs',
-      system,
-      lib,
-      ...
-    }: let
-      treefmtPkg = config.treefmt.build.wrapper;
-    in {
-      # Use `treefmt` for formatting
-      # GitHub: https://github.com/numtide/treefmt-nix
-      # Import treefmt config
-      treefmt.imports = [./treefmt.nix];
+  imports = [
+    inputs.treefmt-nix.flakeModule
+  ];
+  perSystem = {
+    config,
+    pkgs,
+    inputs',
+    system,
+    lib,
+    ...
+  }: let
+    treefmtPkg = config.treefmt.build.wrapper;
+  in {
+    # Use `treefmt` for formatting
+    # GitHub: https://github.com/numtide/treefmt-nix
+    # Import treefmt config
+    treefmt.imports = [./treefmt.nix];
 
-      formatter = treefmtPkg;
-    };
+    formatter = treefmtPkg;
   };
 }
--- flakeModules/formats/treefmt.nix
+++ flakeModules/formats/treefmt.nix
@@ -3,41 +3,55 @@
 # SPDX-FileCopyrightText: 2020-2025 Jörg Kütemeier <https://kuetemeier.de/>
 # SPDX-License-Identifier: MPL-2.0
 #
-{lib, ...}: let
-  inherit (lib) mkDefault;
-in {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   # Used to find the project root
-  projectRootFile = mkDefault "flake.lock";
+  projectRootFile = "flake.lock";
 
-  settings.global.excludes = mkDefault [
+  settings.global.excludes = [
     # No formating and checks for this files
     "LICENSE"
 
     # Ignore some files in the template directories
     "templates/**/LICENSE"
     "templates/**/.gitignore"
+    "templates/**/.editorconfig"
+    "templates/**/justfile"
 
     # Editorconfig settings
     ".editorconfig"
+
+    "*.lock"
+    "*.patch"
+    "package-lock.json"
+    "go.mod"
+    "go.sum"
+    ".gitignore"
+    ".gitmodules"
+    ".hgignore"
+    ".svnignore"
   ];
 
   # Scan Nix files for dead code
   # programs.deadnix.enable = true;
 
   # Our nix file formatter
-  programs.alejandra.enable = mkDefault true;
+  programs.alejandra.enable = true;
 
   # Justfile formatter
-  programs.just.enable = mkDefault true;
+  programs.just.enable = true;
 
   # Format Markdown etc.
-  programs.prettier.enable = mkDefault true;
+  programs.prettier.enable = true;
 
   # Python formatter (replacement for black)
   # programs.ruff.format = true;
   # programs.ruff.check = true;
 
   # Shell script checks
-  # programs.shellcheck.enable = pkgs.hostPlatform.system != "riscv64-linux";
-  # programs.shfmt.enable = pkgs.hostPlatform.system != "riscv64-linux";
+  programs.shellcheck.enable = pkgs.hostPlatform.system != "riscv64-linux";
+  programs.shfmt.enable = pkgs.hostPlatform.system != "riscv64-linux";
 }
