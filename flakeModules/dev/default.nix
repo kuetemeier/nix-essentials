778ac2d5287beebd5877c3f5ff2514924cf5ecfd
Jörg Kütemeier <joerg@kuetemeier.de>
2025-02-25 12:44:10 +0100

    Refactor(dev): Remove flake partition and split up dev shells

--- flakeModules/dev/default.nix
+++ flakeModules/dev/default.nix
@@ -9,49 +9,50 @@ localFlake: {
   inputs,
   ...
 }: {
-  # Extra things to load only when accessing development-specific
-  # attributes such as `checks`
-  partitionedAttrs.devShells = "essentialsDev";
-  partitions.essentialsDev.extraInputsFlake = ./.;
-  partitions.essentialsDev.module = {inputs, ...}: {
-    imports = [
-      inputs.treefmt-nix.flakeModule
-      inputs.nix-unit.modules.flake.default
-    ];
-    perSystem = {
-      config,
-      pkgs,
-      inputs',
-      system,
-      lib,
-      ...
-    }: {
-      # Use `treefmt` for formatting
-      # GitHub: https://github.com/numtide/treefmt-nix
-      # Import treefmt config
-      # treefmt.imports = [./../formats/treefmt.nix];
+  imports = [
+    inputs.treefmt-nix.flakeModule
+    inputs.nix-unit.modules.flake.default
+  ];
+  perSystem = {
+    config,
+    pkgs,
+    inputs',
+    system,
+    lib,
+    ...
+  }: {
+    # TODO: Needs more testing - is treefmt a requirement for dev shell?
+    # Use `treefmt` for formatting
+    # GitHub: https://github.com/numtide/treefmt-nix
+    # Import treefmt config
+    # treefmt.imports = [./../formats/treefmt.nix];
 
-      # formatter = config.treefmt.build.wrapper;
+    # formatter = config.treefmt.build.wrapper;
 
-      devShells.default = let
-        pythonEnv = pkgs.python3.withPackages (_ps: []);
-      in
-        pkgs.mkShell {
-          packages = [
-            pythonEnv
-            pkgs.difftastic
-            config.treefmt.build.wrapper
-            inputs'.nix-unit.packages.default
-            pkgs.just
-            pkgs.editorconfig-checker
-          ];
-          shellHook = ''
-            alias j=just
-            echo $'\e[1;32mWelcome to development Shell~\e[0m'
-            echo -n "Hint: 'just' is aliased to 'j'. "
-            echo "Just run 'j' for a list of possible commands"
-          '';
-        };
+    devShells.default = pkgs.mkShell {
+      packages = [
+        # pythonEnv
+        pkgs.difftastic
+        config.treefmt.build.wrapper
+        inputs'.nix-unit.packages.default
+        pkgs.just
+        pkgs.editorconfig-checker
+      ];
+      shellHook = ''
+        alias j=just
+        echo $'\e[1;32mWelcome to development Shell~\e[0m'
+        echo -n "Hint: 'just' is aliased to 'j'. "
+        echo "Just run 'j' for a list of possible commands"
+      '';
     };
+
+    devShells.python = let
+      pythonEnv = pkgs.python3.withPackages (_ps: []);
+    in
+      pkgs.mkShell {
+        packages = [
+          pythonEnv
+        ];
+      };
   };
 }
